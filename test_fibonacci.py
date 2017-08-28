7c8a14026a77be41e3171f1368991dee42f82f47
Gary Cheng <garycheng@kkbox.com>
2017-08-28 17:10:54 +0800

    Add another test method
    1. Use for loop to check value, it will immediately stop when failure detected

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -3,11 +3,18 @@ from fibonacci import fib
 
 
 class TestFibonacci(unittest.TestCase):
+
+    def setUp(self):
+        self.c = fib()
+        self.expect = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
     def test_fibonacci(self):
-        c = fib()
-        result = [next(c) for _ in range(10)]
-        expect = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-        self.assertListEqual(expect, result)
+        result = [next(self.c) for _ in range(10)]
+        self.assertListEqual(self.expect, result)
+
+    def test_fibonacci2(self):
+        for i in range(10):
+            self.assertEqual(self.expect[i], next(self.c))
 
 
 if __name__ == '__main__':
